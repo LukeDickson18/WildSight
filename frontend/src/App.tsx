27268b4aaaa73903ae5ce6300916f35853752191
Luke Dickson <ldickson1802@gmail.com>
2026-07-02 15:35:28 +0200

    Setup vite and tailwind

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-green-100">
+      <h1 className="text-5xl font-bold text-green-700">
+        WildSight
+      </h1>
+    </div>
+  );
+}
+
+export default App;
